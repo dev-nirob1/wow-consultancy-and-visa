6df08d5cef0c4a31f31744ff2508f21f9172fea1
dev-nirob1 <dev.nirob1@gmail.com>
2026-07-26 14:18:43 +0600

    services page

--- src/app/(front)/services/page.js
+++ src/app/(front)/services/page.js
@@ -1,13 +1,15 @@
 import AllServices from '@/app/components/front/section/AllServices';
+import CtaSection from '@/app/components/front/section/CtaSection';
 import ServicesOverview from '@/app/components/front/section/ServicesOverview';
 import React from 'react';
 
 const ServicesPage = () => {
     return (
-        <div>
+        <>
             <ServicesOverview />
             <AllServices/>
-        </div>
+            <CtaSection/>
+        </>
     );
 };
 
